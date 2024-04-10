498bf9971c15a3757f955c95ae3858769fcf68fe
Carlos Eurípedes dos Santos Neto <137221364+CarlosEuripedesSantosNeto@users.noreply.github.com>
2024-04-10 15:27:27 -0300

    Botão para mudar de tema feito

--- cadastro/cadastro.js
+++ cadastro/cadastro.js
@@ -10,3 +10,20 @@ loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
 
+function myFunction() {
+    var element = document.getElementsByTagName('body')[0];
+    element.classList.toggle("dark-theme");
+    if (element.classList.contains("dark-theme")) {
+        // Se estiver no modo escuro, define a cor de fundo do body como #333
+        element.style.backgroundColor = "#333";
+    } else {
+        // Se não estiver no modo escuro, remove a cor de fundo personalizada
+        element.style.backgroundColor = "";
+    }
+}
+
+const themeToggle = document.getElementById('theme-toggle');
+
+themeToggle.addEventListener('change', () => {
+    document.body.classList.toggle('dark-theme');
+});
